134220fc1a682ea1af60616eddb17b52d47f4aa4
Martin Nowak <code@dawg.eu>
2014-10-08 13:27:02 +0200

    test that deprecation message is still shown for previously imported modules

---
+++ test/compilable/test13582c.d
@@ -0,0 +1,10 @@
+// REQUIRED_ARGS:
+// PERMUTE_ARGS:
+/* TEST_OUTPUT:
+---
+compilable/test13582c.d(9): Deprecation: module imports.imp13582a is deprecated
+compilable/test13582c.d(10): Deprecation: module imports.imp13582b is deprecated
+---
+*/
+import imports.imp13582a;
+import imports.imp13582b;
